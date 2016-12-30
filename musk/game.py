bb85001586de93b01116db5aec45005731f9654f
Steve Clement <steve@localhost.lu>
2016-12-30 10:31:12 +0100

    - Initial commit for the Twin Musk Peaks project

---
+++ musk/game.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# import pgzero wrapper workaround to easily call it from the CLI
+from runpgzero import *
+
+# Expose colors to our namespace
+from colors import *
+
+''' Declarations '''
+
+''' Game Loop '''
+# Event Handling
+def on_mouse_down(pos, button):
+    pass
+
+# Run Logic
+def update():
+    pass
+
+# Update the display
+def draw():
+    pass
+
+# Run the above code
+run_pgzero()
